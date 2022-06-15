0d289410349f9d96403e7e1fe1b61c27068603fc
JTKiew <kiewjt2000@gmail.com>
2022-06-15 21:30:56 +0800

    implement middleware for BearerAuthToken

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,42 +1,33 @@
 import { BadRequestException, Inject, Injectable, UnauthorizedException } from '@nestjs/common';
-import { AuthService } from 'src/auth/auth.service';
 import { userData, Users } from 'src/database';
 import { userSorting } from 'src/Utility/Sorting';
 import { createUserDto, editUserDto, queryUserDto } from './dto';
 
 @Injectable()
 export class UserService {
-    @Inject(AuthService)
-    private authService: AuthService;
-
     // local variable to save the users data 
     private users: Users[] = userData;
 
     // receive the name and email to create new user
     createUser(dto: createUserDto, headers: {}){
-        // verify Bearer token in Header.Authorization
-        if (this.authService.authToken(headers)){
-             // search the users for duplicates name or email
-            let exist = this.users.find((obj) => {
-                return obj.name === dto.name || obj.email === dto.email;
-            })
-            // create user
-            if (exist === undefined || null){
-                // add new users
-                this.users.push({
-                    id: this.users.length , 
-                    name: dto.name,
-                    email: dto.email
-                });
-                console.log(this.users)
-                return `User ${dto.name} created!`
-            }
-            else{
-                throw new BadRequestException("Name or Email already been used!");
-            }
+        // search the users for duplicates name or email
+        let exist = this.users.find((obj) => {
+            return obj.name === dto.name || obj.email === dto.email;
+        })
+        // create user
+        if (exist === undefined || null){
+            // add new users
+            this.users.push({
+                id: this.users.length , 
+                name: dto.name,
+                email: dto.email
+            });
+            console.log(this.users)
+            return `User ${dto.name} created!`
         }
-        else
-            throw new UnauthorizedException();
+        else{
+            throw new BadRequestException("Name or Email already been used!");
+        }
     }
 
     // get user info with given id
@@ -75,47 +66,37 @@ export class UserService {
 
     // edit user's name or email 
     editUser(id: number, dto: editUserDto, headers: {}){
-        // verify Bearer token in Header.Authorization
-        if (this.authService.authToken(headers)){
-             // verify given id
-            if (this.validId(id)){
-                // not blank means have changing towards name or email
-                if (dto.name !== ''){
-                    this.users[id].name = dto.name;
-                }
-                if (dto.email !== ''){
-                    this.users[id].email = dto.email;
-                }
-                console.log(this.users)
-                return `User with id ${id} edited!`
+        // verify given id
+        if (this.validId(id)){
+            // not blank means have changing towards name or email
+            if (dto.name !== ''){
+                this.users[id].name = dto.name;
             }
-            else {
-                throw new BadRequestException("Invalid id! User not exists!");
+            if (dto.email !== ''){
+                this.users[id].email = dto.email;
             }
+            console.log(this.users)
+            return `User with id ${id} edited!`
         }
-        else
-            throw new UnauthorizedException();
+        else {
+            throw new BadRequestException("Invalid id! User not exists!");
+        }
     }
 
     // delete user with given id
     deleteUser(id: number, headers: any){
-        // verify Bearer token in Header.Authorization
-        if (this.authService.authToken(headers)){
-            // verify given id
-            if (this.validId(id)){
-                // delete the user
-                this.users.splice(id,1);
-                // update remaining users's id
-                this.users = userSorting(this.users);
-                console.log(this.users);
-                return `User with id ${id} deleted!`;
-            }
-            else {
-                throw new BadRequestException("Invalid id! User not exists!");
-            }
+        // verify given id
+        if (this.validId(id)){
+            // delete the user
+            this.users.splice(id,1);
+            // update remaining users's id
+            this.users = userSorting(this.users);
+            console.log(this.users);
+            return `User with id ${id} deleted!`;
         }
-        else
-            throw new UnauthorizedException();
+        else {
+            throw new BadRequestException("Invalid id! User not exists!");
+        }
     }
 
     // helper function to verify given id
