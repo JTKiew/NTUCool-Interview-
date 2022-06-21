0b25c007817809724bf0b2923469ef1f629b00ce
JTKiew <kiewjt2000@gmail.com>
2022-06-21 15:57:32 +0800

    fix Restful API routes

--- src/course/course.controller.ts
+++ src/course/course.controller.ts
@@ -2,16 +2,14 @@ import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { CourseService } from './course.service';
 
-@ApiTags('Course')
-@Controller('course')
+@ApiTags('Courses')
+@Controller('courses')
 export class CourseController {
     constructor( private courseService: CourseService) {}
 
-    // get course data by id
-    // url: localhost:3000/course/get/:id
-    // :id must replace by the id of target course
-    @Get('get/:id')
-    getCourse(@Param('id', ParseIntPipe) id: number){
-        return this,this.courseService.getCourse(id);
+    // get Course by courseId
+    @Get(':courseId')
+    getCourse(@Param('courseId', ParseIntPipe) courseId: number){
+        return this,this.courseService.getCourse(courseId);
     }
 }
--- src/database/courseData.ts
+++ src/database/courseData.ts
@@ -1,22 +1,22 @@
 export const courseData = [
     {
-    "id": 1,
+    "id": 0,
     "name": "Software engineering 101"
     },
     {
-    "id": 2,
+    "id": 1,
     "name": "成為 Cool 大師的路上"
     },
     {
-    "id": 3,
+    "id": 2,
     "name": "從零開始的 Cool 之旅"
     },
     {
-    "id": 4,
+    "id": 3,
     "name": "You Don't Know Js"
     },
     {
-    "id": 5,
+    "id": 4,
     "name": "I Don't Know Js yet"
     }
 ]
